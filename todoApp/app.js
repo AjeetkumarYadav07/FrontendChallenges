c27be63a618d72c525c21d27a2b89978f7f5ed4e
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2025-01-07 18:03:46 +0530

    add some new things

--- todoApp/app.js
+++ todoApp/app.js
@@ -1,45 +1,72 @@
 const taskNote = document.querySelector("#goal-note");
 const checBoxes = document.querySelectorAll(".goal-checkbox");
-const  inputField =  document.querySelectorAll(".goal-input");
+const inputField = document.querySelectorAll(".goal-input");
+const progressBar = document.querySelector(".progress-bar") ;
+const progressValue = document.querySelector(".progress-value");
 
- function ErorNotice () {
-     let isError = false;
+/*  Use local storage to sotre the items */
+//    progressValue.style.visibility = "visible";
+//   function updateProgress(){
 
-    inputField.forEach((filed) =>{
-        if(filed.value.trim() === ""){
-            isError = true ;
-           
-        }
-        
-    });
+//   }
 
-    if(isError){
-        taskNote.style.visibility = "visible";
-        return false ;
-       }
-       else{
-        taskNote.style.visibility = "hidden";
-        return true;
-       };
-     
+const storeGoals = JSON.parse(localStorage.getItem("storeGoals")) || {};
 
- 
+function ErorNotice() {
+  let isError = false;
+
+  inputField.forEach((filed) => {
+    if (filed.value.trim() === "") {
+      isError = true;
+    }
+  });
+
+  if (isError) {
+    taskNote.style.visibility = "visible";
+    return false;
+  } else {
+    taskNote.style.visibility = "hidden";
+    return true;
+  }
 }
 
+ /// to set the value on localStrage to broswe again //
 
-
-
- inputField.forEach((filed) =>{
-    filed.addEventListener('input' , () =>{
-        ErorNotice();
-    })
- })
-
-checBoxes.forEach((CheckBox) =>{
-     CheckBox.addEventListener("click" , (e)=>{
-        if(ErorNotice()){
-            CheckBox.parentElement.classList.add('completed')
+inputField.forEach((filed) =>{
+    if(storeGoals[filed.id]){
+        filed.value = storeGoals[filed.id].name;
+        if(storeGoals[filed.id].completed){
+            filed.parentElement.classList.add('completed');
         }
-        
-     });
+    }
 });
+
+inputField.forEach((filed) => {
+  filed.addEventListener("input", () => {
+    ErorNotice();
+  });
+
+  filed.addEventListener("input", (e) => {
+    storeGoals[e.target.id] = {
+      name: filed.value,
+      completed: false,
+    };
+    localStorage.setItem("storeGoals", JSON.stringify(storeGoals));
+  });
+
+});
+
+
+    checBoxes.forEach((CheckBox) => {
+        CheckBox.addEventListener("click", (e) => {
+          if (ErorNotice()) {
+            CheckBox.parentElement.classList.add("completed");
+           
+          }
+        });
+      });
+      
+
+
+  
+ 
